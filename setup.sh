2565dfcdae6b33e927000db8dd13effbdb0ca259
Lewis Treacy <lewisjtreacy@gmail.com>
2016-09-22 22:24:43 +0100

    Setup vim-plug install

--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
+#Add repos and install software
 for i in $(grep -vE "^\s*#" repo_list.txt  | tr "\n" " ")
 do
-	sudo add-apt-repository "ppa:$i"
+    sudo add-apt-repository "ppa:$i"
 done
 sudo apt-get install $(grep -vE "^\s*#" install_list.txt  | tr "\n" " ")
+
+#Download vim-plug
+curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
