c93e58f07ddb32fd91606613e885ed0372283d00
Lewis Treacy <lewisjtreacy@gmail.com>
2016-10-08 14:44:53 +0100

    Moved to vim from nvim

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #Add repos and install software
 for i in $(grep -vE "^\s*#" repo_list.txt  | tr "\n" " ")
 do
-    sudo add-apt-repository "ppa:$i"
+    add-apt-repository "ppa:$i"
 done
-sudo apt-get install $(grep -vE "^\s*#" install_list.txt  | tr "\n" " ")
+apt-get install $(grep -vE "^\s*#" install_list.txt  | tr "\n" " ")
 
 #Download vim-plug
-curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
