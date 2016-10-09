252002ece9114bdd3b86fd0f9f0f84a52bd5eded
Lewis Treacy <lewisjtreacy@gmail.com>
2016-10-09 21:11:32 +0100

    Further changes to move from nvim to vim

--- .aliases.sh
+++ .aliases.sh
@@ -2,8 +2,5 @@ alias sudo='sudo '
 alias ls='ls -la --color=auto'
 alias update='sudo apt-get update && sudo apt-get upgrade'
 
-alias v='nvim'
+alias v='vim'
 alias python='python3.5'
-
-alias matrix='cmatrix'
-alias pipes='. ~/.scripts/pipes.sh'
