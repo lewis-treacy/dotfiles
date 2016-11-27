5026f6e0daf2b1ac3abeb774ff2e0cfefb6b0e7f
Lewis Treacy <lewisjtreacy@gmail.com>
2016-11-27 23:50:36 +0000

    removed aliases nolonger needed

--- .aliases.sh
+++ .aliases.sh
@@ -1,8 +1,6 @@
 alias sudo='sudo '
 alias ls='ls -la --color=auto'
-alias update='sudo apt-get update && sudo apt-get upgrade'
 
 alias v='vim'
+alias vi='vim'
 alias python='python3.5'
-
-alias vpn='cd /etc/openvpn/ && sudo openvpn *.ovpn && cd -'
