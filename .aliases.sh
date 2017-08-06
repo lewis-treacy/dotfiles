a28928e91ba38cefdccd13756315f73a52c84465
Lewis Treacy <lewis.treacy16@imperial.ac.uk>
2017-08-06 23:46:02 +0100

    Removed title from sysinfo

--- .aliases.sh
+++ .aliases.sh
@@ -4,4 +4,4 @@ alias v="nvim"
 alias vi="nvim"
 alias vim="nvim"
 
-alias sysinfo="neofetch --disable theme icons --block_width 4 --block_range 0 15 --colors 1 15 15 1 15 15 --ascii_colors 4 4 --ascii_bold off --bold off && ~/scripts/color-scripts/hex"
+alias sysinfo="neofetch --disable title theme icons --block_width 4 --block_range 0 15 --colors 1 15 15 1 15 15 --ascii_colors 4 4 --ascii_bold off --bold off && ~/scripts/color-scripts/hex"
