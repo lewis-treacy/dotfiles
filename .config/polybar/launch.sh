e0bd31a88d77669130955b230ec9144713ac7fc8
Lewis Treacy <lewis.treacy16@imperial.ac.uk>
2017-09-27 01:38:15 +0100

    changed back to dual monitor desktop

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,9 +6,9 @@ while pgrep -x polybar >/dev/null; do sleep 1; done
 
 if [[ $(hostname) == "GLaDOS" ]]; then
   echo "Launcing desktop bars..."
-  # polybar bar1 &
-  # polybar bar2 &
-  polybar temp -c $HOME/.config/polybar/config_desktop &
+  polybar bar1 -c $HOME/.config/polybar/config_desktop &
+  polybar bar2 -c $HOME/.config/polybar/config_desktop &
+  # polybar temp -c $HOME/.config/polybar/config_desktop &
 elif [[ $(hostname) == "wheatley" ]]; then
   echo "Launcing laptop bars..."
   polybar bar3 -c $HOME/.config/polybar/config_laptop &
