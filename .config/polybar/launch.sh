b57460a96923942fea0b4aba5ea09df38effd7a5
Lewis Treacy <lewis.treacy16@imperial.ac.uk>
2017-10-30 22:45:19 +0000

    disabled clicking scrolling for bspwn module

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,16 +1,15 @@
-#! /usr/bin/env sh
+#!/bin/bash
 
 echo "Killing current bars..."
 killall -q polybar
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
 if [[ $(hostname) == "GLaDOS" ]]; then
-  echo "Launcing desktop bars..."
+  echo "Launching desktop bars..."
   polybar bar1 -c $HOME/.config/polybar/config_desktop &
   polybar bar2 -c $HOME/.config/polybar/config_desktop &
-  # polybar temp -c $HOME/.config/polybar/config_desktop &
 elif [[ $(hostname) == "wheatley" ]]; then
-  echo "Launcing laptop bars..."
+  echo "Launching laptop bars..."
   polybar bar3 -c $HOME/.config/polybar/config_laptop &
 fi
 
