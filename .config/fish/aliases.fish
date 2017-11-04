a9da45f1e329151dce1a8efcb67ebe9ea7f80ad2
Lewis Treacy <lewis.treacy16@imperial.ac.uk>
2017-11-04 21:19:23 +0000

    make dedicated neofetch config

--- .config/fish/aliases.fish
+++ .config/fish/aliases.fish
@@ -5,4 +5,4 @@ alias v vim
 alias vi vim
 alias vim nvim
 
-alias sysinfo "neofetch --disable model title theme icons --block_width 5 --block_height 2 --block_range 0 15 --colors 1 15 15 1 15 15 --ascii_colors 4 4 --ascii_bold off --bold off; bash ~/scripts/color-scripts/hex"
+alias sysinfo "neofetch; hex"
