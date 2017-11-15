35e6c16742ff94f24f249474d367de11102efbd5
Lewis Treacy <lewis.treacy16@imperial.ac.uk>
2017-11-15 18:24:12 +0000

    export and make all env variables global

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,12 +7,12 @@ end
 setxkbmap -option caps:escape
 
 set -U SXHKD_SHELL /bin/bash
-set EDITOR nvim
-set LANG en_GB.UTF-8
-set PATH /usr/local/bin $HOME/.go/bin $HOME/scripts $PATH
-set GOPATH $HOME/.go
-set VISUAL vim
-set SHELL /usr/bin/fish
+set -gx GOPATH $HOME/.go
+set -gx EDITOR vim
+set -gx VISUAL vim
+set -gx LANG en_GB.UTF-8
+set -gx PATH /usr/local/bin $HOME/.go/bin $HOME/scripts $PATH
+set -gx SHELL /usr/bin/fish
 
 set fish_color_autosuggestion brgrey
 set fish_color_command green
