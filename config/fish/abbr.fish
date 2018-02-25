4eae71adcc5e04bbf8a1bf6b1f80d59d8916cb11
Lewis Treacy <lewisjtreacy@gmail.com>
2018-02-25 16:46:00 +0000

    change abbreviations to not use other abbreviation

--- config/fish/abbr.fish
+++ config/fish/abbr.fish
@@ -1,12 +1,12 @@
-abbr -a ls "ls -lh --color=auto"
-abbr -a la "ls -A"
+abbr -a ls "ls -lh"
+abbr -a la "ls -lh -A"
 abbr -a mkdir "mkdir -p"
 abbr -a ya "yaourt"
 abbr -a yas "yaourt -Syua"
 
-abbr -a v vim
-abbr -a vi vim
-abbr -a wim vim
+abbr -a v nvim
+abbr -a vi nvim
+abbr -a wim nvim
 abbr -a vim nvim
 
 abbr -a sysinfo "neofetch; hex"
