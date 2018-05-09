9d83bac0c5258280c87e0d48608a7a60dbef8cdc
Lewis Treacy <lewisjtreacy@gmail.com>
2018-05-09 05:26:58 +0100

    change laptop and desktop hostnames

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -4,11 +4,11 @@ echo "Killing current bars..."
 killall -q polybar
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-if [[ $(hostname) == "GLaDOS" ]]; then
+if [[ $(hostname) == "lewisDesktop" ]]; then
   echo "Launching desktop bars..."
   polybar bar1 -c $HOME/.config/polybar/config_desktop &
   polybar bar2 -c $HOME/.config/polybar/config_desktop &
-elif [[ $(hostname) == "wheatley" ]]; then
+elif [[ $(hostname) == "lewisLaptop" ]]; then
   echo "Launching laptop bars..."
   polybar bar3 -c $HOME/.config/polybar/config_laptop &
 fi
