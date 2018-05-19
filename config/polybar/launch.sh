52f8dfcd8bbd9a9378da2eb5801f4ec982b222e5
Lewis Treacy <lewisjtreacy@gmail.com>
2018-05-19 14:37:53 +0100

    make configs generic

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -4,14 +4,6 @@ echo "Killing current bars..."
 killall -q polybar
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-if [[ $(hostname) == "lewisDesktop" ]]; then
-  echo "Launching desktop bars..."
-  polybar bar1 -c $HOME/.config/polybar/config_desktop &
-  polybar bar2 -c $HOME/.config/polybar/config_desktop &
-  polybar bar3 -c $HOME/.config/polybar/config_desktop &
-elif [[ $(hostname) == "lewisLaptop" ]]; then
-  echo "Launching laptop bars..."
-  polybar bar3 -c $HOME/.config/polybar/config_laptop &
-fi
+# <launch computer specific bars>
 
 echo "Bars launched..."
