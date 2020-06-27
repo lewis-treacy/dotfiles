0b5030ca0b25aceb3ddc6cd46af1e93df43481f1
Lewis Treacy <lewisjtreacy@gmail.com>
2020-06-27 23:48:10 +0100

    update for new monitor layout

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -6,7 +6,7 @@ while pgrep -x polybar >/dev/null; do sleep 1; done
 
 echo "Launching desktop bars..."
 polybar primary -c $HOME/.config/polybar/config &
-polybar top -c $HOME/.config/polybar/config &
+polybar left -c $HOME/.config/polybar/config &
 polybar right -c $HOME/.config/polybar/config &
 
 echo "Bars launched..."
